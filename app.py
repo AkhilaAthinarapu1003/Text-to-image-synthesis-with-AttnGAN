9d0eebdd4b3c12702e36e8012c0fda64f4dea97e
Akhila Athinarapu <athinarapuakhila1999@gmail.com>
2022-06-18 09:02:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,8 +8,8 @@ st.set_page_config(page_title="AttnGAN", initial_sidebar_state="auto")
 st.sidebar.title("Navigation")
 # Add all application here
 
-app.add_app("Demo", demo.demo_gan)
-app.add_app("AttnGAN Explanation", demo.attngan_explained)
+app.add_app("Birds", demo.demo_gan)
+#app.add_app("AttnGAN Explanation", demo.attngan_explained)
 
 
 # The main app
